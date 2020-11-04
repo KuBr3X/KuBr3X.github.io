446cc585b4a5012967e6c77049d756c81170d976
Jakub <dampsjakub5@gmail.com>
2020-11-04 21:11:49 +0100

    White red change for logical

--- logical operator/sketch.js
+++ logical operator/sketch.js
@@ -3,6 +3,8 @@ function setup() {
 }
 function draw() {
   background(220);
+line (200,0,200,400)
+line (0,200,400,200)
 
   let x = mouseX;
   let y = mouseY;
@@ -14,15 +16,15 @@ function draw() {
 
   let xIsSmall = !xIsLarge
 
-  if (xIsLarge) {
-    circleSize = circleSize * 2
-  }
-  if (xIsLarge && yIsLarge) {
+  if (xIsLarge && !yIsLarge) {
     fill("red");
   } else {
     fill("white")
   }
 
+  if (!xIsLarge && yIsLarge) {
+    fill("red");
+  }
   circle(x, y, circleSize)
 
 }
