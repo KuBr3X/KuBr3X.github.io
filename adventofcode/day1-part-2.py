bf1475d215338b79b74d68fe3f2ae44aec6d049f
Jakub <dampsjakub5@gmail.com>
2020-12-10 10:20:03 +0100

    Did day1

--- adventofcode/day1-part-2.py
+++ adventofcode/day1-part-2.py
@@ -14,20 +14,21 @@ def myHelperFunction():
     global lines
     pass
 
-def problemOne():
+def problemTwo():
     global lines
     
-
-
     for a in lines:
         for b in lines:
-            #print(int(a)+int(b))
-            x = int(a)
-            y = int(b)
-            if 2020 == x + y:
-                print(x,y,x+y,x*y)
+            for c in lines:
+                #print(int(a)+int(b))
+                x = int(a)
+                y = int(b)
+                z = int(c)
+                if 2020 == x + y + z:
+                    print(x,y,z,x+y+z,x*y*z)
+                    return
         
 
 if __name__ == "__main__":
     load_data("day1-numbers.txt")
-    problemOne()
+    problemTwo()
